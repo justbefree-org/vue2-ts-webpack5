4b5fc66a40d3b141e9632e12ef077fb300dd0b67
Strong <justbefree@126.com>
2022-04-13 09:54:56 +0800

    支持模块联邦

---
+++ src/bootstrap.ts
@@ -0,0 +1,20 @@
+import "./hooks";
+import { logInfo } from "@/config";
+logInfo();
+import { default as Platform } from "@/core/Platform";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import { customApps } from "@/custom";
+import { Route, NavigationGuardNext } from "vue-router/types/router";
+
+const p = new Platform({ id: "#app", App });
+p.registerRouterHooks(
+  "beforeEach",
+  (from: Route, to: Route, next: NavigationGuardNext) => {
+    console.log(from, to);
+    next();
+  }
+);
+p.install("test");
+p.install(customApps);
+p.startUp();
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,36 @@
 const { defineConfig } = require("@vue/cli-service");
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
 // MaxListenersExceededWarning:Possible EventEmitter memory leak detected.
 require("events").EventEmitter.defaultMaxListeners = 0;
-const publicPath = process.env.NODE_ENV === "production" ? "/" : "/";
+// const publicPath = process.env.NODE_ENV === "production" ? "/" : "/";
+const publicPath = "http://localhost:8080/";
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "train",
+        // library: { type: "umd", name: "train" },
+        // library: { type: "umd" },
+        filename: "remoteEntry.js",
+        exposes: {
+          "./vue2": "./node_modules/vue/dist/vue",
+        },
+        // remotes: {
+        //   flight: "flight@http://localhost:8081/remoteEntry.js",
+        //   hotel: "hotel@http://localhost:8082/remoteEntry.js",
+        //   train: "train@http://localhost:8083/remoteEntry.js",
+        // },
+        // shared: require("./package.json").dependencies,
+        shared: ["vue"],
+      }),
+    ],
+  },
   css: {
     loaderOptions: {
       sass: {
